db424c5d4d47e8f06049b79bdcc43434e1dabd11
Miguel-ADS-86 <miguelSistema.emp@gmail.com>
2023-09-12 12:18:00 -0300

    estudos sobre eventos e adicionando a apostila

--- teste.js
+++ teste.js
@@ -1,14 +1,11 @@
-let btn1 = document.querySelector('#btn1'); //pega o elemento pelo id e adiciona a variavel.
-let p = document.querySelector('p');
-
-function msg(e){
-    console.log('clicou!!');
-    e.stopPropagation();//eu coloco onde eu quero que o evento funcione e pare
-}
-
-btn1.addEventListener('click', msg);
-
-p.addEventListener('click', ()=>{
-   console.log('clicou no paragrafo');
+window.addEventListener("keydown",(e)=>{
+   if(e.key == 'q'){
+      console.log('apertou a letra q');
+   }
 });
 
+window.addEventListener("keyup",(e)=>{
+    if(e.key == 'q'){
+       console.log('soltou o q');
+    }
+ });
